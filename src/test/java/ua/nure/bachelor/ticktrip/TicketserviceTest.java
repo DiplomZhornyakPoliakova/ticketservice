849cd203284e6374b56466b7ad7eb9dcb8562044
Kseniia Poliakova <poliakovak>
2023-04-10 15:17:34 +0000

    Initial commit

---
+++ src/test/java/ua/nure/bachelor/ticktrip/TicketserviceTest.java
@@ -0,0 +1,21 @@
+package ua.nure.bachelor.ticktrip;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class TicketserviceTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
